2e00c8e9f037e359ecb04bd332ed1eae459da4d4
Abraham Jimenez <q6br73e4@duck.com>
2024-10-14 14:12:53 -0600

    Added tools-and-skills component

--- src/app/main/tools-and-skills/tools-and-skills.component.ts
+++ src/app/main/tools-and-skills/tools-and-skills.component.ts
@@ -5,8 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './tools-and-skills.component.html',
-  styleUrl: './tools-and-skills.component.scss'
+  styleUrl: './tools-and-skills.component.scss',
 })
 export class ToolsAndSkillsComponent {
-
+  skills: string[] = [
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'React.js',
+    'Node.js',
+    'MongoDB',
+    'GraphQL',
+    'Next.js',
+    'TypeScript',
+    'Git/Github',
+    'API',
+    'Tailwind',
+    'Figma',
+    'Trello',
+    'Jira Software',
+    'PostgreSQL',
+    'InDesign',
+    'Illustrator',
+    'Photoshop',
+    'Spanish (fluent)',
+  ];
 }
