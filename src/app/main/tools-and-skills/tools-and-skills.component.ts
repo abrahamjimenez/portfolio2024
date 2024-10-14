32618e2e80eb357e6a42061f38ab59b22b0ad573
Abraham Jimenez <q6br73e4@duck.com>
2024-10-14 13:38:41 -0600

    Added components

---
+++ src/app/main/tools-and-skills/tools-and-skills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tools-and-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './tools-and-skills.component.html',
+  styleUrl: './tools-and-skills.component.scss'
+})
+export class ToolsAndSkillsComponent {
+
+}
