45a50dfa84d26d42b8817d8883f3eb9a6b48dc7d
Abraham Jimenez <q6br73e4@duck.com>
2024-10-14 13:38:41 -0600

    Added components

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+import { ToolsAndSkillsComponent } from './tools-and-skills/tools-and-skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolsAndSkillsComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
