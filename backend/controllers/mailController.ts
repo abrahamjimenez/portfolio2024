0c0923e1eca0f4e40f13c7e6f006c63b19c31202
Abraham Jimenez <q6br73e4@duck.com>
2024-10-20 09:28:53 -0600

    00018 backend works with nodemailer

---
+++ backend/controllers/mailController.ts
@@ -0,0 +1,40 @@
+import {Request, Response} from "express";
+import type {Options} from "nodemailer/lib/mailer";
+import type {SentMessageInfo} from "nodemailer/lib/smtp-transport";
+
+const asyncHandler = require("express-async-handler")
+const nodemailer = require("nodemailer")
+
+const sendMail = asyncHandler(async (req: Request, res: Response) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 456,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL,
+      pass: process.env.GOOGLE_PASSWORD,
+    },
+  });
+
+  const mailOptions: Options = {
+    from: process.env.GMAIL,
+    to: "abraham.jimenez.romo@proton.me",
+    subject: "Hello from Nodemailer",
+    text: "This is a test email sent using Nodemailer"
+  }
+
+  transporter.sendMail(mailOptions, (error: Error | null, info: SentMessageInfo) => {
+    if (error) {
+      console.error("Error sending email: ", error)
+      res.status(500).json({message: "Error sending email"})
+    } else {
+      console.log("Email sent: ", info.response)
+      res.status(200).json({message: "Email sent successfully"})
+    }
+  })
+})
+
+module.exports = {
+  sendMail
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,13 @@
 const express = require("express")
+require("dotenv").config()
+
+const mailRoutes = require("./routes/mailRoutes")
+
 const app = express()
 const port = 3000
 
+app.use("/api/mail", mailRoutes)
 
+app.listen(port, () => {
+  console.log(`Server is running in port: ${port}`)
+})
